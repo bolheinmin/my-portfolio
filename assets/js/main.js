5f0922b21f9c8ffef38b4b5d99e57534f3fc6bc8
bolheinmin <bolheinmin@gmial.com>
2020-10-24 21:39:29 +0630

    work section added

--- assets/js/main.js
+++ assets/js/main.js
@@ -152,15 +152,15 @@
       itemSelector: '.portfolio-item'
     });
 
-    $('#portfolio-flters li').on('click', function() {
-      $("#portfolio-flters li").removeClass('filter-active');
-      $(this).addClass('filter-active');
+    // $('#portfolio-flters li').on('click', function() {
+    //   $("#portfolio-flters li").removeClass('filter-active');
+    //   $(this).addClass('filter-active');
 
-      portfolioIsotope.isotope({
-        filter: $(this).data('filter')
-      });
-      aos_init();
-    });
+    //   portfolioIsotope.isotope({
+    //     filter: $(this).data('filter')
+    //   });
+    //   aos_init();
+    // });
 
     // Initiate venobox (lightbox feature used in portofilo)
     $('.venobox').venobox({
